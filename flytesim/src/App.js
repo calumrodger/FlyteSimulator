9977923cec40dd1fde0f20325041791df16bd266
Ryan Caulfield <ryancaulfield3103@gmail.com>
2022-04-04 15:09:03 +0100

    added sprites for example landing page

--- flytesim/src/App.js
+++ flytesim/src/App.js
@@ -1,27 +1,42 @@
 import "./App.css";
 import NavBar from "./NavBar";
-import RubberCharacter from "./img/RubberCharacter.png";
-import PencilCharacter from "./img/PencilCharacter.png";
+import RubberCapstone from "./img/rubbercapstone.png";
+import PencilCapstone from "./img/pencilcapstone.png";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <h1>Hello!</h1>
-      <div className="sprites">
+      <h2>Hello and welcome to...</h2>
+      <h1>FLYTE SIMULATOR</h1>
+      <Sprites>
         <img
           className="pencilcharacter"
-          src={PencilCharacter}
+          src={PencilCapstone}
           alt="Pencil Character"
         />
         <img
           className="rubbercharacter"
-          src={RubberCharacter}
+          src={RubberCapstone}
           alt="Rubber Character"
         />
-      </div>
+      </Sprites>
     </div>
   );
 }
 
 export default App;
+
+const Sprites = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px;
+  img{
+    width: 500px;
+    height: 500px;
+    padding: 0px 10px 0px 10px;
+  }
+  `
