f015b2a5104791e9ad13bcfc529d72c38f108990
Ryan Caulfield <ryancaulfield3103@gmail.com>
2022-04-04 14:52:26 +0100

    added sprites to app

--- flytesim/src/App.js
+++ flytesim/src/App.js
@@ -1,12 +1,25 @@
-import './App.css';
-import NavBar from './NavBar';
+import "./App.css";
+import NavBar from "./NavBar";
+import RubberCharacter from "./img/RubberCharacter.png";
+import PencilCharacter from "./img/PencilCharacter.png";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <h1>Hello!</h1>
-      
+      <div className="sprites">
+        <img
+          className="pencilcharacter"
+          src={PencilCharacter}
+          alt="Pencil Character"
+        />
+        <img
+          className="rubbercharacter"
+          src={RubberCharacter}
+          alt="Rubber Character"
+        />
+      </div>
     </div>
   );
 }
