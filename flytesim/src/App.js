7348f5c9b16a22da7ecb3ea5bc54af011ebfa1cf
Ryan Caulfield <ryancaulfield3103@gmail.com>
2022-04-05 10:36:10 +0100

    added marquee footer to display scores later

--- flytesim/src/App.js
+++ flytesim/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import NavBar from './NavBar';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <h1>Hello!</h1>
-      
+      <Footer />
     </div>
   );
 }
