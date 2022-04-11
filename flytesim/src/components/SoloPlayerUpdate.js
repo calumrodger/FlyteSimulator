5a3c5ef574bbc4c5f54d9655631360f7ba7fa973
calumrodger <94742384+calumrodger@users.noreply.github.com>
2022-04-11 14:57:15 +0100

    it works mwahahaha

---
+++ flytesim/src/components/SoloPlayerUpdate.js
@@ -0,0 +1,63 @@
+import React, {useState, useEffect, Fragment} from 'react';
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+const SoloPlayerUpdate = ({handleRapPost, score, soloPlayer, setSoloPlayer, soloLineOne, soloLineTwo, onUpdate}) => {
+
+    // const [stateRap, setStateRap] = useState({
+    //     rap: "",
+    //     score: 0
+    // })
+
+    // useEffect(() => {
+    //     if(soloPlayer){
+    //         let copiedPlayer = {...soloPlayer}
+    //         setStatePlayer(copiedPlayer);
+    //     }
+    // }, [soloPlayer])
+
+    const handleDatabaseSubmit = (event) => {
+        event.preventDefault();
+        console.log(soloPlayer.previousRaps)
+        let fullLineString = `${soloLineOne} ${soloLineTwo}`
+        // setSoloPlayer(soloPlayer)
+        let newRap = {
+            rap: fullLineString,
+            score: score,
+            player: soloPlayer
+        }
+        console.log(newRap)
+        handleRapPost(newRap)
+        let updatedPlayer = {
+        name: soloPlayer.name,
+        stageName: soloPlayer.stageName,
+        points: soloPlayer.points += score,
+        previousRaps: [...soloPlayer.previousRaps, newRap]
+        }
+        // let copiedPlayer = {...soloPlayer}
+        // let runningScore = soloPlayer.points
+        // let newScore = runningScore += score
+        // console.log(copiedPlayer)
+        // copiedPlayer.points = newScore
+
+        
+        // let rapArray = [updatedPlayer.previousRaps]
+        // rapArray.push(`${fullLineString}`)
+        // console.log(rapArray)
+        // updatedPlayer.previousRaps = rapArray
+
+        setSoloPlayer(updatedPlayer)
+        onUpdate(soloPlayer);
+        // console.log(statePlayer)
+    }
+
+    return(
+        
+        <>
+        <h3>SAVE TO DATABASE</h3>
+        <button type="submit" onClick={handleDatabaseSubmit}>SAVE</button>
+        </>
+    )
+
+}
+
+export default SoloPlayerUpdate;
