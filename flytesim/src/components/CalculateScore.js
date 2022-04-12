ab6826969ea486f701b53ecc7abf3a579f9a3c11
calumrodger <94742384+calumrodger@users.noreply.github.com>
2022-04-12 14:29:37 +0100

    got alliteration and assonance score bonuses calculated

--- flytesim/src/components/CalculateScore.js
+++ flytesim/src/components/CalculateScore.js
@@ -4,9 +4,6 @@ import styled from "styled-components"
 
 const CalculateScore = ({soloLineOne, soloLineTwo}) => {
 
-    let vowels = []
-    let consonants = []
-
     let fullLineString = `${soloLineOne} ${soloLineTwo}`
     let fullLineArray = fullLineString.split(" ")
     let newArray = []
@@ -35,9 +32,42 @@ const CalculateScore = ({soloLineOne, soloLineTwo}) => {
     let finalArray = uniqueArray.concat(uniqueChars)
     console.log(finalArray)
 
+    let arrayOfArrays = []
+    for (let i = 0; i < finalArray.length; i++) {
+        let miniArray = (finalArray[i].split(" "))
+        arrayOfArrays.push(miniArray)
+      }
+    console.log(arrayOfArrays)
 
-   
+    let alliterationArray = [];
 
+    for (let i = 0; i < arrayOfArrays.length; i++) {
+        alliterationArray.push(arrayOfArrays[i][0])
+    }
+    console.log(alliterationArray);
+
+    let checkUniqueAlliterations = alliterationArray.filter((element, index) => {
+        return alliterationArray.indexOf(element) === index;
+        })
+        console.log(checkUniqueAlliterations)
+
+    let alliterationScore = (alliterationArray.length - checkUniqueAlliterations.length)
+    console.log(alliterationScore)
+
+    let vowels = ["AA", "AE", "AH", "AO", "AW", "AX", "AXR", "AY", "EH", "ER", "EY", "IH", "IX", "IY", "OW", "OY", "UH", "UW", "UX"]
+
+    var merged = [].concat.apply([], arrayOfArrays);
+    console.log(merged)
+    const vowelArray = merged.filter(element => vowels.includes(element));
+    console.log(vowelArray)
+
+    let assonanceArray = vowelArray.filter((element, index) => {
+        return vowelArray.indexOf(element) !== index;
+        });
+    console.log(assonanceArray)
+
+    let assonanceScore = assonanceArray.length
+    console.log(assonanceScore)
 
     return(
         <PlayerRap>
